31418c8a2b427442408bcde11135f29059413de3
Muhamad Arijal <muhammadarijal03@gmail.com>
2026-02-09 09:16:22 +0700

    Add project files.

---
+++ DashboardMonitoringWB/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardMonitoringWB.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
