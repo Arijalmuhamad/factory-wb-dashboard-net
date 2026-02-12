e8fe99be8b8d1522cedb1ae76b44074bbac5d168
Muhamad Arijal <muhammadarijal03@gmail.com>
2026-02-12 17:48:03 +0700

    Menampilkan data informasi penimbangan dari databases

--- DashboardMonitoringWB/Controllers/HomeController.cs
+++ DashboardMonitoringWB/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using DashboardMonitoringWB.Data;
 using DashboardMonitoringWB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 namespace DashboardMonitoringWB.Controllers
 {
@@ -26,10 +28,54 @@ namespace DashboardMonitoringWB.Controllers
 			var culture = new System.Globalization.CultureInfo("id-ID");
 			ViewBag.Today = DateTime.Now.ToString("dddd, dd-MM-yyyy", culture);
 
-			return View();
+
+            DashboardSummary summary = GetDashboardSummary();
+			return View(summary);
 		}
 
-        public IActionResult Privacy()
+        private DashboardSummary GetDashboardSummary()
+        {
+            var result = new DashboardSummary();
+
+			// Query SQL Anda (disesuaikan agar kompetibel dengan ADO.NET)
+
+			string query = @"
+            SELECT 
+                SUM(regis_in = 'T') as regist_in,
+                SUM(weighing_in = 'T') as weighing_in,
+                SUM(weighing_out = 'T') as weighing_out,
+                SUM(regis_out = 'T') as regist_out
+            FROM (
+                SELECT regis_in, weighing_in, weighing_out, regis_out, dateout FROM wb_automation_datatbs_tab
+                UNION ALL
+                SELECT regis_in, weighing_in, weighing_out, regis_out, dateout FROM wb_automation_datacpo_tab
+                UNION ALL
+                SELECT regis_in, weighing_in, weighing_out, regis_out, dateout FROM wb_automation_producttrans_tab
+                UNION ALL
+                SELECT regis_in, weighing_in, weighing_out, regis_out, dateout FROM wb_automation_data_tab
+            ) AS combined_data
+            WHERE DATE(dateout) = IF(HOUR(NOW()) >= 7, CURDATE(), DATE_SUB(CURDATE(), INTERVAL 1 DAY));";
+
+			using (var command = _context.Database.GetDbConnection().CreateCommand())
+			{
+				command.CommandText = query;
+				_context.Database.OpenConnection();
+				using (var reader = command.ExecuteReader())
+				{
+					if (reader.Read())
+					{
+						result.RegistIn = reader.IsDBNull(0) ? 0 : Convert.ToInt32(reader[0]);
+						result.WeighingIn = reader.IsDBNull(1) ? 0 : Convert.ToInt32(reader[1]);
+						result.WeighingOut = reader.IsDBNull(2) ? 0 : Convert.ToInt32(reader[2]);
+						result.RegistOut = reader.IsDBNull(3) ? 0 : Convert.ToInt32(reader[3]);
+					}
+				}
+			}
+			return result;
+        
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
---
+++ DashboardMonitoringWB/Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DashboardMonitoringWB.Models
+{
+    public class DashboardSummary
+	{
+        public int RegistIn { get; set; }
+        public int WeighingIn { get; set; }
+        public int WeighingOut { get; set; }
+		public int RegistOut { get; set; }
+	}
+}
